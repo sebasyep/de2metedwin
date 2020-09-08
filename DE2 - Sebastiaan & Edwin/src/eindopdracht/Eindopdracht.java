5138e2150f1829f53e9cf3b0dcb990b7fe0291bb
Sebastiaan Yepez Miranda <61823910+sebasyep@users.noreply.github.com>
2020-09-08 11:49:46 +0200

    Update Eindopdracht.java

--- DE2 - Sebastiaan & Edwin/src/eindopdracht/Eindopdracht.java
+++ DE2 - Sebastiaan & Edwin/src/eindopdracht/Eindopdracht.java
@@ -1,21 +1,60 @@
 package eindopdracht;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Eindopdracht {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
 		// Woorden worden hier aangemaakt.
 		String[] woorden = { "banaan", "appel", "vuur", "bij", "jas", "pakket", "uien", "bier", "fles", "leren", "teer",
 				"werken", "stift", "woord", "vijf", "zak", "oor", "mond", "kast", "regen", "zon", "kaas", "slim",
 				"deur", "raam", "pot", "zeef", "aarde", "zoen", "spel" };
 
+		
+		boolean inGame = true;
 		// Hier wordt een specifiek woord uitgekozen.
-		int nummer = new Random().nextInt(woorden.length);
-		String gekozenWoord = woorden[nummer];
-
-		System.out.println(gekozenWoord);
+			while(inGame) {
+				int nummer = new Random().nextInt(woorden.length);
+				char randomWoord[] = woorden[nummer].toCharArray();
+				int hoeveelheidKeuzes = randomWoord.length;
+				char spelerKiest[] = new char[hoeveelheidKeuzes];
+				
+				for(int i=0; i<spelerKiest.length; i++){ // De dashes die er komen te staan "_ _ _ _ _ _ _ _"
+					spelerKiest[i] =  '_';
+	            }
+				boolean woordIsGeraden = false;
+	            int pogingen = 0;
+	            
+	            while(!woordIsGeraden && pogingen != hoeveelheidKeuzes) {
+	            	System.out.println("Het woord: ");
+	            	print(spelerKiest);
+	            	System.out.printf("Je hebt %d aantal pogingen over.\n", hoeveelheidKeuzes-pogingen);
+	            	char input = scanner.nextLine().charAt(0);
+	                pogingen++;
+	                
+	                if(input == '-'){
+	                	woordIsGeraden = true;
+	                	inGame = false;
+	                }else{
+	                    for(int i=0; i<randomWoord.length; i++){
+	                        if(randomWoord[i] == input){
+	                        	spelerKiest[i] = input;
+	                        }
+	                    }
+	            }
+			}
+	            
 	}
+  }
 
+	public static void print(char array[]){
+        for(int i=0; i<array.length; i++){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
