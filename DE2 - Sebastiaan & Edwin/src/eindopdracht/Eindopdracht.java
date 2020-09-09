2ce7ec9fe2d922d91e016a681c796b521d3ae6a1
EdwinAsselman <70939838+EdwinAsselman@users.noreply.github.com>
2020-09-09 20:56:22 +0200

    Update Eindopdracht.java
    Alles is nu met noties uitgelegd

--- DE2 - Sebastiaan & Edwin/src/eindopdracht/Eindopdracht.java
+++ DE2 - Sebastiaan & Edwin/src/eindopdracht/Eindopdracht.java
@@ -13,10 +13,12 @@ public class Eindopdracht {
 				"werken", "stift", "woord", "vijf", "zak", "oor", "mond", "kast", "regen", "zon", "kaas", "slim",
 				"deur", "raam", "pot", "zeef", "aarde", "zoen", "spel" };
 
+		// Hier wordt de game opgestart.
 		boolean inGame = true;
 		boolean gewonnen = false;
 
-		// Hier wordt een specifiek woord uitgekozen.
+		// Hier wordt een specifiek woord uitgekozen en ook wordt er bijgehouden welke
+		// letters de gebruiker heeft ingevoerd.
 		int nummer = new Random().nextInt(woorden.length);
 		char randomWoord[] = woorden[nummer].toCharArray();
 		char spelerKiest[] = new char[randomWoord.length];
@@ -32,6 +34,8 @@ public class Eindopdracht {
 			}
 			int pogingen = 5;
 
+			// Hier wordt gecontroleert of de gebruiker geen pogingen meer heeft en of het
+			// woord nog niet geraden is.
 			while (!woordIsGeraden && pogingen != 0) {
 
 				if (geraden != randomWoord.length) {
@@ -39,6 +43,8 @@ public class Eindopdracht {
 					print(spelerKiest);
 					System.out.printf("Je hebt %d aantal pogingen over.\n", pogingen);
 
+					// Als de speler zijn eerste letter heeft ingevoerd, wordt dat daarna hier elke
+					// keer bijgehouden.
 					if (pogingen != 5) {
 						System.out.print("Gekozen letters: ");
 						for (int i = 0; i < letters.length; i++) {
@@ -50,12 +56,16 @@ public class Eindopdracht {
 				}
 				char input = scanner.nextLine().charAt(0);
 
+				// Als de invoer van de gebruiker overeenkomt met 1 van de letters in het
+				// willekeurige gekozen woord, dan worden die letters zichtbaar.
 				for (int i = 0; i < randomWoord.length; i++) {
 					if (randomWoord[i] == input) {
 						spelerKiest[i] = input;
 						geraden++;
 					}
 
+					// Als alle letters zijn geraden dan beindigt het spel hier, en wordt je
+					// overgebracht naar het eindscherm gewonnen.
 					if (geraden == randomWoord.length) {
 						woordIsGeraden = true;
 						inGame = false;
@@ -70,12 +80,16 @@ public class Eindopdracht {
 				telling++;
 
 			}
+			// Als de gebruiker geen pogingen meer heeft en het woord is niet op tijd
+			// geraden, dan beendigt het spel hier, en wordt je overgebracht naar het
+			// eindscherm verloren.
 			if (!woordIsGeraden && pogingen == 0) {
 				inGame = false;
 				gewonnen = false;
 				pogingen = 0;
 			}
 
+			// Wanneer de gebruiker heeft gewonnen.
 			if (gewonnen) {
 				System.out.print("Je hebt gewonnen!\n Het woord was: ");
 				for (int i = 0; i < randomWoord.length; i++) {
@@ -84,7 +98,8 @@ public class Eindopdracht {
 				System.out.println("\n\nWilt u nog een spel starten?\nvoer in: ja of nee");
 				Scanner scanner2 = new Scanner(System.in);
 				String keuze = scanner2.nextLine();
-
+				
+				// De gebruiker heeft hier de keuze om een nieuw spel te beginnen of om te stoppen.
 				if (keuze.equals("ja")) {
 					inGame = true;
 					woordIsGeraden = false;
@@ -96,6 +111,8 @@ public class Eindopdracht {
 					telling = 0;
 					letters = new char[randomWoord.length + 2];
 				}
+
+				// Wanneer de gebruiker heeft verloren.
 			} else {
 				System.out.print("Je hebt verloren..\n Het woord was: ");
 				for (int i = 0; i < randomWoord.length; i++) {
@@ -104,7 +121,8 @@ public class Eindopdracht {
 				System.out.println("\n\nWilt u nog een spel starten?\nvoer in: ja of nee");
 				Scanner scanner2 = new Scanner(System.in);
 				String keuze = scanner2.nextLine();
-
+				
+				// De gebruiker heeft hier de keuze om een nieuw spel te beginnen of om te stoppen.
 				if (keuze.equals("ja")) {
 					inGame = true;
 					woordIsGeraden = false;
